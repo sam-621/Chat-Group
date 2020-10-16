ab5e66464e14b4368506cc43e0b52410a6540bbf
rogeliosamuel621 <rogeliosamuel621@gmail.com>
2020-10-16 08:03:54 -0600

    [ADD] Drop dawn menu components

---
+++ src/components/atoms/DropDawnMenu.jsx
@@ -0,0 +1,20 @@
+import React, { useState } from 'react';
+import { Redirect } from 'react-router-dom';
+import '../../styles/atoms/dropDawnMenu.css';
+
+const DropDawnMenu = () => {
+  const [redirect, setRedirect] = useState(false);
+  function logOut() {
+    setRedirect(true);
+  }
+
+  return redirect ? (
+    <Redirect to="/" />
+  ) : (
+    <div className="DropDawnMenu">
+      <p onClick={logOut}>Log out</p>
+    </div>
+  );
+};
+
+export default DropDawnMenu;
--- src/components/molecules/ChatHeader.jsx
+++ src/components/molecules/ChatHeader.jsx
@@ -1,17 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../../styles/molecules/chatHeader.css';
 
 import { Title } from '../atoms/Text';
+import DropDawnMenu from '../atoms/DropDawnMenu';
 import Options from '../../img/options.svg';
 
 const ChatHeader = () => {
+  const [showMenu, setShowMenu] = useState(false);
   return (
     <header className="ChatHeader">
       <div>
         <Title content="Chat group" />
       </div>
       <div>
-        <img src={Options} alt="Options image" width="30" height="30" />
+        <img
+          onClick={() => setShowMenu(!showMenu)}
+          src={Options}
+          alt="Options image"
+          width="30"
+          height="30"
+        />
+        {showMenu ? <DropDawnMenu setShowMenu={setShowMenu} /> : null}
       </div>
     </header>
   );
