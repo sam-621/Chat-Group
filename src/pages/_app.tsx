37f3f1217533e506aa3e23c4682d214d3624d780
Rogelio Samuel Moreno Corrales <rogeliosamuel621@gmail.com>
2021-12-18 11:51:42 -0700

    integrate Nav component

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,13 @@
 import 'tailwindcss/tailwind.css'
 import type { AppProps } from 'next/app'
+import { Nav } from '@components/containers/Nav'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Nav />
+      <Component {...pageProps} />
+    </>
+  )
 }
 export default MyApp
