2df15f381008491c6b1e8a256d578b1471b3d748
Rogelio Samuel Moreno Corrales <rogeliosamuel621@gmail.com>
2021-12-18 00:07:22 -0700

    add colors to tailwind configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,15 @@
 module.exports = {
   content: ['./src/pages/**/*.{js,ts,jsx,tsx}', './src/common/components/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {}
+    extend: {
+      colors: {
+        body: '#121214',
+        primary: '#FFCD1E',
+        'form-primary': '#1B873F',
+        'form-secondary': '#29292E',
+        'send-message': '#FF008E'
+      }
+    }
   },
   plugins: []
 }
