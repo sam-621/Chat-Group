f4b64e4ff3b512068c7f3658ef2312cf2abf1b71
Rogelio Samuel Moreno Corrales <rogeliosamuel621@gmail.com>
2021-12-18 11:38:06 -0700

    colors changed

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,11 +3,14 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        body: '#121214',
-        primary: '#FFCD1E',
-        'form-primary': '#1B873F',
-        'form-secondary': '#29292E',
-        'send-message': '#FF008E'
+        body: '#FFF',
+        primary: '#615EF0',
+        secondary: '#F1F1F1',
+        'text-black': '#464646',
+        'custom-gray': '#29292E',
+        'input-border': '#E2E8F0',
+        'error-text': '#F10E0E',
+        'error-bg': 'rgba(241, 14, 14, 0.1)'
       }
     }
   },
