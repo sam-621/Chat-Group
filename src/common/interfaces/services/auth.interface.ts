fa8c2a3ef7fabdf975bc0aef5feed414dd855666
Rogelio Samuel Moreno Corrales <rogeliosamuel621@gmail.com>
2021-12-18 21:11:30 -0700

    add ILoginResponse

--- src/common/interfaces/services/auth.interface.ts
+++ src/common/interfaces/services/auth.interface.ts
@@ -1,4 +1,11 @@
 import { IUser } from '@interfaces/user.interface'
 
+// Dto
 export type TUserLoginDto = Pick<IUser, 'email' | 'password'>
 export type TUserRegisterDto = Omit<IUser, 'profilePic'>
+
+// Responses
+export interface ILoginResponse {
+  token: string
+  user: Omit<IUser, 'password'> & { _id: string }
+}
