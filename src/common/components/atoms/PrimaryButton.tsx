3021b6c99e013ec13498ebadb39792b3835ebeb4
Rogelio Samuel Moreno Corrales <rogeliosamuel621@gmail.com>
2021-12-24 00:14:11 -0700

    add PrimaryButton component

---
+++ src/common/components/atoms/PrimaryButton.tsx
@@ -0,0 +1,13 @@
+import { ButtonHTMLAttributes, DetailedHTMLProps, FC } from 'react'
+
+export const PrimaryButton: FC<Props> = ({ children, onClick, className }) => {
+  return (
+    <button className={`bg-primary text-white rounded ${className}`} onClick={onClick}>
+      {children}
+    </button>
+  )
+}
+
+type Props = DetailedHTMLProps<ButtonHTMLAttributes<HTMLButtonElement>, HTMLButtonElement> & {
+  onClick?: () => void
+}
--- src/common/components/containers/WelcomeCard.tsx
+++ src/common/components/containers/WelcomeCard.tsx
@@ -1,3 +1,4 @@
+import { PrimaryButton } from '@components/atoms/PrimaryButton'
 import { Strong } from '@components/atoms/Strong'
 import { DEFAULT_USER_IMAGE } from '@helpers/images'
 import Image from 'next/image'
@@ -8,10 +9,11 @@ export const WelcomeCard = () => {
       <div className="flex justify-center">
         <Image src={DEFAULT_USER_IMAGE} width={200} height={200} alt="User" />
       </div>
-      <div>
+      <div className="flex flex-col gap-10">
         <p className="text-center">
           Send <Strong>messages</Strong> in real time and meet new <Strong>people</Strong>
         </p>
+        <PrimaryButton className="py-5">Get in with email</PrimaryButton>
       </div>
     </div>
   )
