428bdf7ca4cf85e2e68d01fc8e49ae7d647848e1
Rogelio Samuel Moreno Corrales <rogeliosamuel621@gmail.com>
2021-12-28 01:11:40 -0700

    add THandleSubmit type

--- src/common/interfaces/utils.interface.ts
+++ src/common/interfaces/utils.interface.ts
@@ -1,5 +1,9 @@
+import { FormEvent } from 'react'
+
 export type TInputType = 'text' | 'email' | 'password'
 
 export type THashMap<T = string> = {
   [index: string]: T
 }
+
+export type THandleSubmit = (e: FormEvent<HTMLFormElement>) => void
