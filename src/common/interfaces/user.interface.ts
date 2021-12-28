38802457e0d45d9b569137dfca15600fce880440
Rogelio Samuel Moreno Corrales <rogeliosamuel621@gmail.com>
2021-12-28 00:26:36 -0700

    add user types

--- src/common/interfaces/user.interface.ts
+++ src/common/interfaces/user.interface.ts
@@ -4,3 +4,6 @@ export interface IUser {
   password: string
   profilePic?: string
 }
+
+export type TRegisterUser = Omit<IUser, 'profilePic'>
+export type TLoginUser = Omit<IUser, 'profilePic' | 'username'>
