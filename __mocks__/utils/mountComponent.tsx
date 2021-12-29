fb1c9f6721ab019752aac9430f6d1d283476b42c
Rogelio Samuel Moreno Corrales <rogeliosamuel621@gmail.com>
2021-12-29 13:01:55 -0700

    update mountComponent

--- __mocks__/utils/mountComponent.tsx
+++ __mocks__/utils/mountComponent.tsx
@@ -1,9 +1,27 @@
+import { AuthProvider } from '@contexts/Auth.context'
 import { act, render, RenderOptions, waitFor } from '@testing-library/react'
+import { ToastContainer } from 'react-toastify'
 
 export const mountComponent = async (Component: JSX.Element, options?: RenderOptions) => {
   await waitFor(() => {
     act(() => {
-      render(<>{Component}</>, options)
+      render(
+        <AuthProvider>
+          {Component}{' '}
+          <ToastContainer
+            position="bottom-left"
+            autoClose={5000}
+            hideProgressBar={false}
+            newestOnTop={false}
+            closeOnClick
+            rtl={false}
+            pauseOnFocusLoss
+            draggable
+            pauseOnHover
+          />
+        </AuthProvider>,
+        options
+      )
     })
   })
 }
