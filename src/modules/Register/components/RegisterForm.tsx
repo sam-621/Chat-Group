4786bec0d285570188fa711c7e6971185329d01c
Rogelio Samuel Moreno Corrales <rogeliosamuel621@gmail.com>
2021-12-28 23:34:41 -0700

    add useRegisterForm

--- src/modules/Register/components/RegisterForm.tsx
+++ src/modules/Register/components/RegisterForm.tsx
@@ -1,26 +1,15 @@
 import { Input } from '@components/atoms/Input'
 import { PrimaryButton } from '@components/atoms/PrimaryButton'
-import { useHandleAuth } from '@hooks/useHanldeAuth'
+import { useHandleAuth } from '@hooks/useHandleAuth'
 import { useInput } from '@hooks/useInput'
-import { TUserRegisterDto } from '@interfaces/services/auth.interface'
-import { FormEvent } from 'react'
+import { useRegisterForm } from '../hooks/useRegisterForm'
 
 export const RegisterForm = () => {
   const username = useInput('text')
   const email = useInput('email')
   const password = useInput('password')
   const { register } = useHandleAuth()
-
-  const user: TUserRegisterDto = {
-    email: email.value,
-    password: password.value,
-    username: username.value
-  }
-
-  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
-    e.preventDefault()
-    register(user)
-  }
+  const { handleSubmit } = useRegisterForm(username, email, password, (user) => register(user))
 
   return (
     <form onSubmit={handleSubmit} className="px-5 grid gap-5">
