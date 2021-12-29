6da51fbb8451946395d2de21d01c93ba368b9648
Rogelio Samuel Moreno Corrales <rogeliosamuel621@gmail.com>
2021-12-29 13:29:57 -0700

    add isValidEmail helper

--- src/common/helpers/auth.ts
+++ src/common/helpers/auth.ts
@@ -4,3 +4,8 @@ import { getCookie } from '@libs/cookies'
 export const getIsLogged = () => Boolean(getCookie(TOKEN_FIELD))
 export const getToken = () => getCookie(TOKEN_FIELD)
 export const authRoutes = ['/login', '/register']
+
+export const isValidEmail = (email: string) => {
+  const regExp = new RegExp(/.+\@.+\.[a-zA-Z]+/gi)
+  return regExp.test(email)
+}
