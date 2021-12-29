283bff4ba26a6a25bd907a4d5973c7fac4bc520a
Rogelio Samuel Moreno Corrales <rogeliosamuel621@gmail.com>
2021-12-28 22:33:01 -0700

    add config to swr!

--- src/common/constants/cache.constants.ts
+++ src/common/constants/cache.constants.ts
@@ -1,4 +1,7 @@
 // SWR
+export const INTERVAL_TO_REFRESH = 60000 // 1 minute
+
+// CACHE
 export const USER_PROFILE = 'USER_PROFILE'
 
 // BROWSER
--- src/common/hooks/fetch/useUser.ts
+++ src/common/hooks/fetch/useUser.ts
@@ -1,9 +1,12 @@
 import { getUserProfile } from '@apis/user.api'
-import { USER_PROFILE } from '@constants/cache.constants'
+import { INTERVAL_TO_REFRESH, USER_PROFILE } from '@constants/cache.constants'
 import useSWR from 'swr'
 
 export const useUser = () => {
-  const { data, error } = useSWR(USER_PROFILE, getUserProfile)
+  const { data, error } = useSWR(USER_PROFILE, getUserProfile, {
+    revalidateOnFocus: false,
+    refreshInterval: INTERVAL_TO_REFRESH
+  })
 
   return {
     data,
