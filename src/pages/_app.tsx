0420b43d869f5a02c9105337653d5d5ea798d2ab
Rogelio Samuel Moreno Corrales <rogeliosamuel621@gmail.com>
2021-12-28 22:59:05 -0700

    add react-toastify

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,24 @@
 import '../styles/main.css'
+import { ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
 import type { AppProps } from 'next/app'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    // <AuthProvider>
-    <Component {...pageProps} />
-    // </AuthProvider>
+    <>
+      <Component {...pageProps} />
+      <ToastContainer
+        position="bottom-left"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+      />
+    </>
   )
 }
 
