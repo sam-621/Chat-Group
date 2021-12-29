1ab8482d43528b675e759349ae30d7e3416b7e0a
Rogelio Samuel Moreno Corrales <rogeliosamuel621@gmail.com>
2021-12-29 00:11:34 -0700

    add UserCardInformation component

--- src/common/components/containers/WelcomeCard.tsx
+++ src/common/components/containers/WelcomeCard.tsx
@@ -1,22 +1,17 @@
-import { PrimaryLink } from '@components/atoms/PrimaryLink'
-import { Strong } from '@components/atoms/Strong'
+import { UserCardInformation } from '@components/molecules/UserCardInformation'
 import { DEFAULT_USER_IMAGE } from '@helpers/images'
+import { useUser } from '@hooks/fetch/useUser'
 import Image from 'next/image'
 
 export const WelcomeCard = () => {
+  const { data, isLoading } = useUser()
+
   return (
     <div className="p-12">
       <div className="flex justify-center">
-        <Image src={DEFAULT_USER_IMAGE} width={200} height={200} alt="User" />
+        <Image src={data?.profilePic || DEFAULT_USER_IMAGE} width={200} height={200} alt="User" />
       </div>
-      <div className="flex flex-col gap-10">
-        <p className="text-center">
-          Send <Strong>messages</Strong> in real time and meet new <Strong>people</Strong>
-        </p>
-        <PrimaryLink href="/register" className="py-5">
-          Get in with email
-        </PrimaryLink>
-      </div>
+      {isLoading ? <h1>Loading</h1> : <UserCardInformation />}
     </div>
   )
 }
---
+++ src/common/components/molecules/UserCardInformation.tsx
@@ -0,0 +1,30 @@
+import { PrimaryLink } from '@components/atoms/PrimaryLink'
+import { Strong } from '@components/atoms/Strong'
+import { useUser } from '@hooks/fetch/useUser'
+import { useAuth } from '@hooks/useAuth'
+
+export const UserCardInformation = () => {
+  const { data } = useUser()
+  const { isLogged } = useAuth()
+  return (
+    <>
+      {isLogged ? (
+        <div className="flex flex-col gap-5">
+          <h2 className="text-text-black text-center text-2xl">@{data?.username}</h2>
+          <PrimaryLink href="/edit/profile" className="py-4">
+            Edit profile
+          </PrimaryLink>
+        </div>
+      ) : (
+        <div className="flex flex-col gap-10">
+          <p className="text-center">
+            Send <Strong>messages</Strong> in real time and meet new <Strong>people</Strong>
+          </p>
+          <PrimaryLink href="/register" className="py-5">
+            Get in with email
+          </PrimaryLink>
+        </div>
+      )}
+    </>
+  )
+}
