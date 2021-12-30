e6bcece5784b03d6285de6b81797edf087f22481
Rogelio Samuel Moreno Corrales <rogeliosamuel621@gmail.com>
2021-12-30 12:40:23 -0700

    add isInvalidImage helper

--- src/common/helpers/images.ts
+++ src/common/helpers/images.ts
@@ -1,1 +1,11 @@
 export const DEFAULT_USER_IMAGE = '/icons/default-user.svg'
+
+export const isInvalidImage = (extension: string) => {
+  return (
+    extension !== '.png' &&
+    extension !== '.jpg' &&
+    extension !== '.svg' &&
+    extension !== '.jpeg' &&
+    extension !== '.ico'
+  )
+}
