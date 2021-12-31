abed67eceed82ed7e1a509575a8d5921373ab100
Rogelio Samuel Moreno Corrales <rogeliosamuel621@gmail.com>
2021-12-31 14:08:07 -0700

    add ImageService

---
+++ src/common/services/image.service.ts
@@ -0,0 +1,19 @@
+import { CLOUD_API_DOMAIN, CLOUD_NAME } from '@constants/env.constants'
+import { ServiceResponse } from '@helpers/service-response'
+import HttpRequest from './http-request.service'
+
+const URL_PREFIX = 'v1_1'
+
+export class ImageService extends HttpRequest {
+  async upload(data: FormData) {
+    this.configRequest({
+      endpoint: `${URL_PREFIX}/${CLOUD_NAME}/image/upload`,
+      apiDomain: CLOUD_API_DOMAIN
+    })
+
+    const response = await this.post(data)
+    console.log(response)
+
+    return new ServiceResponse(response.data, response)
+  }
+}
