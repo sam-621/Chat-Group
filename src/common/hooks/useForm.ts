6c0a711c1d93fc42607af67e70d7d5049e779f98
Rogelio Samuel Moreno Corrales <rogeliosamuel621@gmail.com>
2022-01-02 16:59:23 -0700

    add useForm hook

--- src/common/hooks/useForm.ts
+++ src/common/hooks/useForm.ts
@@ -1,68 +1,55 @@
 import { isValidEmail } from '@helpers/auth'
-import { useState } from 'react'
 import { TUseInput } from './useInput'
 
 export const useForm: TUseForm = ({ username, email, password }) => {
-  const [isFormOk, setIsFormOk] = useState(false)
-  const { setError: setUsernameError } = username
-  const { setError: setEmailError } = email
-  const { setError: setPasswordError } = password
-
   const usernameExists = Boolean(username)
   const emailExists = Boolean(email)
   const passwordExists = Boolean(password)
 
   const cleanErrors = () => {
-    setEmailError('')
-    setPasswordError('')
-    setUsernameError('')
+    username?.setError('')
+    email?.setError('')
+    password?.setError('')
   }
-  const validateFormData = () => {
+  const isValidFormData = () => {
     cleanErrors()
 
-    if (usernameExists && !username.value) {
-      setUsernameError('Username should not be empty')
-      return
+    if (usernameExists && !username?.value) {
+      username?.setError('Username should not be empty')
+      return false
     }
 
-    if (emailExists && !email.value) {
-      setEmailError('Email must not be empty')
-      return
+    if (emailExists && !email?.value) {
+      email?.setError('Email must not be empty')
+      return false
     }
 
-    if (emailExists && !isValidEmail(email.value)) {
-      setEmailError('Email format is not correct')
-      return
+    if (emailExists && !isValidEmail(email?.value || '')) {
+      email?.setError('Email format is not correct')
+      return false
     }
 
-    if (passwordExists && !password.value) {
-      setPasswordError('Password must not be empty')
-      return
+    if (passwordExists && (password?.value?.length || 0) < 6) {
+      password?.setError('Password must be bigger than 5 characters')
+      return false
     }
 
-    if (passwordExists && password.value.length < 6) {
-      setPasswordError('Password must be bigger than 5 characters')
-      return
-    }
-
-    setIsFormOk(true)
+    return true
   }
 
   return {
-    isFormOk,
-    validateFormData
+    isValidFormData
   }
 }
 
 type TUseForm = {
   (props: TUseFormProps): {
-    isFormOk: boolean
-    validateFormData: () => void
+    isValidFormData: () => boolean
   }
 }
 
 type TUseFormProps = {
-  username: TUseInput
-  email: TUseInput
-  password: TUseInput
+  username?: TUseInput
+  email?: TUseInput
+  password?: TUseInput
 }
--- src/modules/Login/hooks/useLoginForm.ts
+++ src/modules/Login/hooks/useLoginForm.ts
@@ -1,45 +1,20 @@
-import { isValidEmail } from '@helpers/auth'
+import { useForm } from '@hooks/useForm'
 import { TUseInput } from '@hooks/useInput'
 import { TUserLoginDto } from '@interfaces/services/auth.interface'
 import { THandleSubmit } from '@interfaces/utils.interface'
 
 export const useLoginForm: TUseLoginForm = (email, password, cb) => {
-  const { setError: setEmailError } = email
-  const { setError: setPasswordError } = password
+  const { isValidFormData } = useForm({ email, password })
 
   const userToSave: TUserLoginDto = {
     email: email.value,
     password: password.value
   }
 
-  const cleanErrors = () => {
-    setEmailError('')
-    setPasswordError('')
-  }
-
   const handleSubmit: THandleSubmit = (e) => {
     e.preventDefault()
-    cleanErrors()
 
-    if (!email.value) {
-      setEmailError('Email must not be empty')
-      return
-    }
-
-    if (!isValidEmail(email.value)) {
-      setEmailError('Email format is not correct')
-      return
-    }
-
-    if (!password.value) {
-      setPasswordError('Password must not be empty')
-      return
-    }
-
-    if (password.value.length < 6) {
-      setPasswordError('Password must be bigger than 5 characters')
-      return
-    }
+    if (!isValidFormData()) return
 
     cb(userToSave)
   }
