c76cf2d7ba489bde78c26dad740cf8a72b6a6026
Rogelio Samuel Moreno Corrales <rogeliosamuel621@gmail.com>
2022-01-02 17:34:22 -0700

    add updateUserProfile api

--- src/common/apis/user.api.ts
+++ src/common/apis/user.api.ts
@@ -1,10 +1,24 @@
-import { TUserProfileResponse } from '@interfaces/services/user.interface'
+import {
+  TEditProfileDto,
+  TEditProfileResponse,
+  TUserProfileResponse
+} from '@interfaces/services/user.interface'
 import { UserService } from '@services/user.service'
 
 const userService = new UserService()
 
 export const getUserProfile = async (): Promise<TUserProfileResponse> => {
   const { data, response } = await userService.getUserProfile()
+
+  if (!data) {
+    throw response
+  }
+
+  return data
+}
+
+export const updateUserProfile = async (user: TEditProfileDto): Promise<TEditProfileResponse> => {
+  const { data, response } = await userService.updateUserProfile(user)
 
   if (!data) {
     throw response
