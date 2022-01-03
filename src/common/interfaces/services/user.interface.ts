fcd9766f4d6e250351c7d154ea495d5d3a7ded14
Rogelio Samuel Moreno Corrales <rogeliosamuel621@gmail.com>
2022-01-02 17:27:43 -0700

    add TEditProfileDto

--- src/common/interfaces/services/user.interface.ts
+++ src/common/interfaces/services/user.interface.ts
@@ -1,5 +1,8 @@
 import { IUser } from '@interfaces/user.interface'
 
+// Dto
+export type TEditProfileDto = Omit<IUser, 'password'>
+
 // Responses
 export type TUserProfileResponse = Omit<IUser, 'password'> & {
   _id: string
