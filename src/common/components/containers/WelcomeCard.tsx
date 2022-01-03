69d14ccb511ea99b9e09b45557c90861241dca5b
Rogelio Samuel Moreno Corrales <rogeliosamuel621@gmail.com>
2022-01-02 18:02:48 -0700

    fix bug about logout

--- src/common/components/containers/WelcomeCard.tsx
+++ src/common/components/containers/WelcomeCard.tsx
@@ -1,17 +1,20 @@
 import { Loader } from '@components/atoms/loaders/Loader'
 import { UserCardInformation } from '@components/molecules/UserCardInformation'
+import { useAuthContext } from '@contexts/Auth.context'
 import { DEFAULT_USER_IMAGE } from '@helpers/images'
 import { useUser } from '@hooks/fetch/useUser'
 import Image from 'next/image'
 
 export const WelcomeCard = () => {
   const { data, isLoading } = useUser()
+  const { isLogged } = useAuthContext()
+  const userImage = isLogged ? data?.profilePic || DEFAULT_USER_IMAGE : DEFAULT_USER_IMAGE
 
   return (
     <div className="p-12">
       <div className="flex justify-center">
         <Image
-          src={data?.profilePic || DEFAULT_USER_IMAGE}
+          src={userImage}
           width={200}
           height={200}
           alt="User"
