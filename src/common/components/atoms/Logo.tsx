d62aec34e061bcc4f457242fab2be6574002b63a
Rogelio Samuel Moreno Corrales <rogeliosamuel621@gmail.com>
2022-01-08 14:48:39 -0700

    add link for logo component

--- src/common/components/atoms/Logo.tsx
+++ src/common/components/atoms/Logo.tsx
@@ -1,3 +1,11 @@
+import Link from 'next/link'
+
 export const Logo = () => {
-  return <h1 className="font-roboto text-primary font-medium text-2xl md:text-3xl">Chat Group</h1>
+  return (
+    <Link href="/">
+      <a href="">
+        <h1 className="font-roboto text-primary font-medium text-2xl md:text-3xl">Chat Group</h1>
+      </a>
+    </Link>
+  )
 }
